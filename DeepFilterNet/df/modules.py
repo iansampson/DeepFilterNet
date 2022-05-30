7f03bfa66232c1dfcb4b5db1ea7131513978db2d
Ian Sampson <iansampson87@gmail.com>
2022-05-30 11:24:47 -0700

    Add code for tracing and converting model to CoreML

--- DeepFilterNet/df/modules.py
+++ DeepFilterNet/df/modules.py
@@ -721,7 +721,9 @@ class GroupedLinearEinsum(nn.Module):
 
     def forward(self, x: Tensor) -> Tensor:
         # x: [..., I]
-        x = x.unflatten(-1, (self.groups, self.ws))  # [..., G, I/G]
+        new_shape = list(x.shape)[:-1] + list((self.groups, self.ws)) + list(x.shape)[:0]
+        x = x.view(*new_shape)
+        # x = x.unflatten(-1, (self.groups, self.ws))  # [..., G, I/G]
         x = torch.einsum("...gi,...gih->...gh", x, self.weight)  # [..., G, H/G]
         x = x.flatten(2, 3)  # [B, T, H]
         return x
